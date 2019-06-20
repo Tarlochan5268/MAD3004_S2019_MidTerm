15fbe8434420d2fa13cdbe8625f57208c468bd92
Tarlochan <brotherstarlochan5268@gmail.com>
2019-06-20 18:45:31 -0400

    [Tarlochan] made bill class

---
+++ C0751142_MID_TERM_MAD3004/C0751142_MID_TERM_MAD3004/Mobile.swift
@@ -0,0 +1,24 @@
+//
+//  Mobile.swift
+//  C0751142_MID_TERM_MAD3004
+//
+//  Created by admin on 2019-06-20.
+//  Copyright © 2019 Tarlochan5268. All rights reserved.
+//
+
+import Foundation
+class Mobile : Bill
+{
+    var mobileModelName : String
+    var mobileNumber : String
+    var internetGBused : Float
+    var minuteUsed : Int
+    
+    override init()
+    {
+        self.mobileModelName = ""
+        self.mobileNumber = ""
+        self.internetGBused = 0
+        self.minuteUsed = 0
+    }
+}
